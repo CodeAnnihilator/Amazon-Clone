ab0543ab5b2608937e5d23970932c6348a61a5f5
Eugene <frostberryart@gmail.com>
2016-11-30 10:09:54 +0300

    registration has been created

---
+++ config/secret.js
@@ -0,0 +1,5 @@
+module.exports = {
+  database: 'mongodb://root:Delisian1@ds111788.mlab.com:11788/ecommerce',
+  port: 3001,
+  secretKey: 'Code$$'
+}
---
+++ server.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var engine = require('ejs-mate');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var flash = require('express-flash');
+var MongoStore = require('connect-mongo')(session);
+var passport = require('passport');
+
+var secret = require('./config/secret');
+var User = require('./models/user');
+
+var app = express();
+
+mongoose.connect(secret.database, function(err) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log('Connected to the database');
+  }
+})
+
+// Middleware
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: secret.secretKey,
+  store: new MongoStore({
+    url: secret.database,
+    autoReconnect: true
+  })
+}));
+app.use(flash());
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+
+var mainRoutes = require('./routes/main');
+var userRoutes = require('./routes/user');
+app.use(mainRoutes);
+app.use(userRoutes);
+
+
+
+app.listen(secret.port, function(err) {
+  if(err) throw err;
+  console.log('Server is running on port ' + secret.port);
+})
